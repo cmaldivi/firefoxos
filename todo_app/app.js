e2c4d3ec59471c2964bf66876d738f0b934e7040
cmaldivi <christophe.maldivi@gmail.com>
2015-09-23 17:55:01 +0200

    minor

--- todo_app/app.js
+++ todo_app/app.js
@@ -1,3 +1,9 @@
 window.addEventListener("load", function() {
   console.log("Hello World!");
 });
+
+function storeNewTask(){
+     var newTask= document.getElementById("newtask");
+     localStorage.setItem("newtask", newTask.value);
+     console.log("new Task: " + newTask.value);
+}
